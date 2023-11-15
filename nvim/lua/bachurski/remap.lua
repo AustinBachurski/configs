bdd3d92fc09afbed9772a3467e5dd8ca1544ac35
Austin Bachurski <58963315+AustinBachurski@users.noreply.github.com>
2023-11-15 09:37:19 -0700

    Update remap.lua

--- nvim/lua/bachurski/remap.lua
+++ nvim/lua/bachurski/remap.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>ft", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gvzz")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gvzz")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
@@ -17,5 +17,7 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+vim .keymap.set("n", "<C-d>", "<C-d>zz")
+vim .keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>/", vim.cmd.nohl)
