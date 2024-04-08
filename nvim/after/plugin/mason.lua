af23232d54cb260d1d4564c92b5a53ede0a3c9d4
Austin Bachurski <58963315+AustinBachurski@users.noreply.github.com>
2024-04-08 08:54:49 -0600

    Update mason.lua

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,9 +1,22 @@
 require("mason-lspconfig").setup {
     ensure_installed = {
-        "clangd",
-        "csharp_ls",
-        "quick_lint_js",
-        "pyright",
-        "lua_ls",
+        "bashls", -- Bash
+        "clangd", -- C++
+        "cmake", -- CMake
+        "csharp_ls", -- C#
+        "cssls", -- CSS
+        "eslint", -- JavaScript
+        "html", -- HTML
+        "java_language_server", -- Java
+        "jsonls", -- Json
+        "lua_ls", -- Lua
+        "marksman", -- Markdown
+        "pyright", -- Python
+        "rust_analyzer", -- Rust
+        "taplo", -- TOML
+        "tsserver", -- TypeScript
+        "sqlls", -- SQL
+        "yamlls", -- YAML
+        "zls", -- Zig
     },
 }
